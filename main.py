48e7bc82365c4c42276ae207b5641554e73ed397
Devin Thomas <devin.thomas20@imperial.ac.uk>
2022-10-19 20:40:16 +0100

    Better reading

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ import sys
 def read_dataset(
     filepath: str,
 ) -> Tuple[np.ndarray, np.ndarray]:
-    x = []
-    y = []
-    for line in open(filepath):
-        if line.strip() != "":
-            row = line.strip().split("\t")
-            x.append(list(map(float, row[:-1])))
-            y.append(row[-1])
-
-    return (np.array(x), np.array(y))
+    all = np.loadtxt(filepath)
+    x = all[:, :-1]
+    y = all[:, -1]
+    return (x, y)
 
 def decision_tree_learning(matrix: np.ndarray, depth: int):
     pass
